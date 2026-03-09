cb24425bb78a9fe3f6959e86c48e02be62d23b4f
GarrettC72 <garrettchen2@gmail.com>
2026-03-08 23:37:37 -0400

    Updated server to create AMQP connection and publish message to queue

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	connectionString := "amqp://guest:guest@localhost:5672/"
+
+	connection, err := amqp.Dial(connectionString)
+	if err != nil {
+		log.Fatalf("could not connect to RabbitMQ: %v", err)
+	}
+	defer connection.Close()
+	fmt.Println("Peril game server connected to RabbitMQ!")
+
+	channel, err := connection.Channel()
+	if err != nil {
+		log.Fatalf("failed to create channel: %v", err)
+	}
+	err = pubsub.PublishJSON(
+		channel,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+		routing.PlayingState{
+			IsPaused: true,
+		},
+	)
+	if err != nil {
+		log.Printf("could not publish time: %v", err)
+	}
+	fmt.Println("Pause message sent!")
 }
